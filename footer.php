977c8c3e8c44c9a4e581ab01576a61266491ac02
Ayush Sekhari <as3663@cornell.edu>
2017-09-05 15:03:51 -0400

    Add initial files for ACL 2018 website.

---
+++ footer.php
@@ -0,0 +1,14 @@
+
+      <div class=banner_bottom>
+      <div class=banner_bottom_text>
+        <center>
+          <h4>Kyoto, Japan, 15-17 October 2017</h4>
+        </center>
+      </div> <!-- end banner_text -->
+      </div> <!-- end banner -->   
+
+<? php
+
+	echo 'hello'
+
+>
